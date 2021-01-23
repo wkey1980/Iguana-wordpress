daf149cac5a2a7a516831b3b40ec88e5d96c92c5
Will <28814921+wkey1980@users.noreply.github.com>
2021-01-23 18:08:10 +0000

    added wordpress loop

--- index.php
+++ index.php
@@ -6,6 +6,29 @@
 
 <?php get_header(); ?>
 
+    <!-- Main Section -->
+    <main id="main" class="main">
 
+        <!-- MordPress Loop -->
+        <?php if ( have_posts() ) : ?>
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <article class="post">
+                    <?php the_title(); ?>
+                    <?php the_content(); ?>
+                </article>
+
+            <?php endwhile ?>
+
+        <?php else : ?>
+
+            <article class="post error">
+                <h1 class="404">No posts found</h1>
+            </article>
+
+        <?php endif; ?>
+
+    </main>
 
 <?php get_footer(); ?>
