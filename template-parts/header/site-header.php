257428fbc879b866c7a60a7656f1e96a467fcdd9
Will <28814921+wkey1980@users.noreply.github.com>
2021-01-25 12:39:31 +0000

    theme structure setup - header

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -3,3 +3,18 @@
  * @package iguana
  */
 ?>
+
+<!-- Header Section -->
+<header id="header" class="header">
+
+    <!-- Site Navigation -->
+    <div class="site-navigation">
+        <?php get_template_part( 'template-parts/header/site-navigation' ); ?>
+    </div>
+
+    <!-- Site Branding -->
+    <div class="site-branding">
+    <?php get_template_part( 'template-parts/header/site-branding' ); ?>
+    </div>
+
+</header>
