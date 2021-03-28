10283c69967a5b7518b50fd2ca955a0acbd19446
will k <28814921+wkey1980@users.noreply.github.com>
2021-03-28 16:15:18 +0100

    commit

--- inc/menus.php
+++ inc/menus.php
@@ -11,10 +11,10 @@
 function iguana_register_menu() {
     register_nav_menus(
         array(
-            'header' => __( 'Header Menu', 'uguana' ),
-            'footer' => __( 'Footer Menu', 'uguana' ),
-            'mobile' => __( 'Mobile Menu', 'uguana' ),
-            'social' => __( 'Social Menu', 'uguana' ),
+            'header' => __( 'Header Menu', 'iguana' ),
+            'footer' => __( 'Footer Menu', 'iguana' ),
+            'mobile' => __( 'Mobile Menu', 'iguana' ),
+            'social' => __( 'Social Menu', 'iguana' ),
         )
     );
 }
